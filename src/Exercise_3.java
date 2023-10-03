8832aa15aaf88a05333d49877346dbf146db55c3
Andrea Cavallo <astarore@gmail.com>
2023-10-03 14:23:42 +0200

    add exercise 3

---
+++ src/Exercise_3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercise_3 {
+    public static void main(String[] args) {
+        String text1 = "";
+        Scanner input = new Scanner(System.in);
+        while (!(text1.equals(":q"))) {
+            System.out.println("dammi in pasto una stringa");
+            text1 = input.nextLine();
+            splitString(text1);
+        }
+    }
+    public static void splitString (String t1) {
+        for (int i=0; i<t1.length(); i++) {
+            System.out.println(t1.charAt(i) + ",");
+        }
+    }
+}
